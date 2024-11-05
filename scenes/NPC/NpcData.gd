ca61a3adb61d40ca367c5768f2b2750f9562ef74
Kits <luck3801@gmail.com>
2024-11-04 21:19:37 -0300

    aaaaaaaaaa

---
+++ scenes/NPC/NpcData.gd
@@ -0,0 +1,6 @@
+extends Resource
+
+class_name NpcData;
+
+@export var name: String;
+@export var Srite: Texture2D;
---
+++ scenes/NPC/npc_data.gd
@@ -0,0 +1,7 @@
+extends Resource
+
+class_name NpcData;
+
+@export var name: String;
+@export var Srite: Texture2D;
+@export_multiline var Text: String;
